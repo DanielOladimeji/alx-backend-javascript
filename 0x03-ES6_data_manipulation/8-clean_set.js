169dd91f8f2abe4d35358982a9b9a9d0c74d7aae
DanielOladimeji <danieloladimejiblessing@gmail.com>
2023-10-04 19:14:23 -0700

    Corrected

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,24 +1,16 @@
-export default function cleanSet(set: Set<string>, startString: string): string {
-  // Validate set and startString
+export default function cleanSet(set, startString) {
+  const parts = [];
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-
-  let result = '';
-
-  for (const value of set) {
+  for (const value of set.values()) {
     if (typeof value === 'string' && value.startsWith(startString)) {
       const valueSubStr = value.substring(startString.length);
 
-      // Only add to the result if the value after removing the prefix is non-empty and different from the original value
       if (valueSubStr && valueSubStr !== value) {
-        if (result) {
-          result += '-';
-        }
-        result += valueSubStr;
+        parts.push(valueSubStr);
       }
     }
   }
-
-  return result;
+  return parts.join('-');
 }
