46082aa3c057de8f4a2b74da0c6a369d27eb7c39
DanielOladimeji <danieloladimejiblessing@gmail.com>
2023-10-05 18:04:25 -0700

    Task Done

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,38 +1,49 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.checkType(name, 'string', 'Name must be a string');
-    this._length = this.checkType(length, 'number', 'Length must be a number');
-    this._students = this.checkType(students, Array.isArray, 'must be an array');
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
+  // Gets the name of this course.
   get name() {
     return this._name;
   }
 
-  set name(n) {
-    this._name = this.checkType(n, 'string', 'Name must be a string');
+  // Sets the name of this course.
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be strings');
+    }
+    this._name = value;
   }
 
+  // Gets the length of this course (in months).
   get length() {
     return this._length;
   }
 
-  set length(size) {
-    this._length = this.checkType(size, 'number', 'Length must be a number');
+  // Sets the length of this course (in months).
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be numbers');
+    }
+    this._length = value;
   }
 
+  // Gets the names of students in this course.
   get students() {
     return this._students;
   }
 
-  set students(stu) {
-    this._students = this.checkType(stu, Array.isArray, 'must be an array');
-  }
-
-  checkType(value, type, errorMsg) {
-    if (!type(value)) {
-      throw new TypeError(errorMsg);
+  // Sets the names of students in this course.
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array containing strings');
     }
-    return value;
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array containing strings');
+    }
+    this._students = value;
   }
 }
